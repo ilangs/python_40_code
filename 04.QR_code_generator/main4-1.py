22fcd69d52d25eac69093962fcf9bff318b276da
ilangs <ilangs2011@gmail.com>
2025-12-27 00:25:33 +0900

    12/26 study and modified

--- 04.QR_code_generator/main4-1.py
+++ 04.QR_code_generator/main4-1.py
@@ -1,10 +1,10 @@
 import qrcode
 
 # QR 코드에 넣을 데이터
-data = "Hello, world!"
+data = "https://dirt-cheddar-57f.notion.site/4-Team_project_-2d32114b6f5c801198deeb8dea1a4466?source=copy_link"
 
 # QR 코드 생성
 img = qrcode.make(data)
 
 # 이미지 파일로 저장
-img.save("qrcode.png")
+img.save("qr_proj.png")
